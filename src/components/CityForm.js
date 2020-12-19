b1c61b7a36fcb50a8b13cf30596c7741c9264835
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-20 05:45:49 +0800

    Increase size of main search field

--- src/components/CityForm.js
+++ src/components/CityForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, Row, Col } from 'react-bootstrap';
 
 const CityForm = (props) => {
     const { cityHandler, isMain } = props;
@@ -18,9 +18,11 @@ const CityForm = (props) => {
                 </Form>
             }
             {isMain &&
-                <Form inline onSubmit={cityHandler(searchText)}>
-                    <FormControl type="text" defaultValue="" onChange={updateSearchText} placeholder="Enter a city..." className="mr-sm-2" />
-                    <Button variant="outline-primary" type="submit">Search</Button>
+                <Form size="lg" inline onSubmit={cityHandler(searchText)}>
+                    <Form.Row>
+                        <FormControl size="lg" type="text" defaultValue="" onChange={updateSearchText} placeholder="Enter a city..." className="mr-sm-2" />
+                        <Button size="lg" variant="outline-primary" type="submit">Search</Button>
+                    </Form.Row>
                 </Form>
             }
         </>
