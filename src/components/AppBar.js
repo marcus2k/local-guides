41e2fce691865026db9e366f2b68a4c9659eeadb
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-20 03:13:37 +0800

    Add basic navbar component

---
+++ src/components/AppBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const AppBar = (props) => {
+    return (
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Local Guides</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+            <NavDropdown title="Settings" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Guide Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" disabled>Inbox</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" disabled>Bookings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Enter a city..." className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default AppBar;
