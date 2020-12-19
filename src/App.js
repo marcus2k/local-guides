9aac0f199b41ed82223c3f31b5d1a15a4c915352
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-20 03:17:51 +0800

    Implement dummy auth states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AppBar from './components/AppBar'
 
 function App() {
+  const [ isAuthed, setAuth ] = useState(false);
+
+  const login = () => {
+    setAuth(true);
+  }
+
+  const logout = () => {
+    setAuth(false);
+  }
+
+  
+  
   return (
     <div className="App">
-      <AppBar />
+      <AppBar 
+      isAuthed={isAuthed}
+      loginHandler={login}
+      logoutHandler={logout}
+      />
       {/*<header className="App-header">
       </header>*/}
     </div>
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 const AppBar = (props) => {
+    const { isAuthed, loginHandler, logoutHandler } = props;
+
     return (
         <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">Local Guides</Navbar.Brand>
@@ -9,14 +11,16 @@ const AppBar = (props) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Guide Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2" disabled>Inbox</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" disabled>Bookings</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-            </NavDropdown>
+            { !isAuthed && <Nav.Link onClick={loginHandler}>Login</Nav.Link> }
+            { isAuthed &&
+                <NavDropdown title="Settings" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Guide Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" disabled>Inbox</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3" disabled>Bookings</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+            }
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Enter a city..." className="mr-sm-2" />
