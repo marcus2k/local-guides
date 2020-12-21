c34ba669b4e293c6f9634f1a5674ef86a88ea8a9
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-21 09:38:45 +0800

    Use alert for name and mobiile validation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,10 @@ import Select from 'react-select';
 
 const sortThenObjectify = lst => lst.sort((a, b) => a > b ? 1 : -1).map(x => ({value: x, label: x}));
 
+const NAME_ERROR = "Name must be at least 3 characters."
+
+const MOBILE_ERROR = "Mobile must consist of at least 8 numeric characters."
+
 const CITIES_ERROR = "You must select at least one city."
 
 const LANG_ERROR = "You must select at least one language."
@@ -37,6 +41,7 @@ const Profile = (props) => {
     const [ citiesError, setCitiesError ] = useState(false); // at least one
     const [ langError, setLangError ] = useState(false); // at least one
     const [ showSuccess, setSuccess ] = useState(false);
+    const [ showError, setError ] = useState(false);
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -62,10 +67,14 @@ const Profile = (props) => {
         isValidMobile(mobile) ? setMobileError(false) : setMobileError(true);
         const isSuccess = isValidName(name) && isValidCities(cities) && isValidLanguages(languages) && isValidCities(mobile);
         isSuccess ? setSuccess(true) : setSuccess(false);
+        isSuccess ? setError(false) : setError(true);
         setTimeout(() => {
             setLangError(false);
             setCitiesError(false);
-            setSuccess(false)
+            setNameError(false);
+            setCitiesError(false);
+            setSuccess(false);
+            setError(false);
         }, 5000);
         isSuccess ? setFormState(newProfile) : setFormState({...formState});
         console.log(name);
@@ -80,15 +89,17 @@ const Profile = (props) => {
 
     return (
         <>
-            {(citiesError || langError) && 
+            {showError && 
             <Alert variant="danger">
+                {nameError && <div>{NAME_ERROR}</div>}
+                {mobileError && <div>{MOBILE_ERROR}</div>}
                 {citiesError && <div>{CITIES_ERROR}</div>}
                 {langError && <div>{LANG_ERROR}</div>}
             </Alert>
             }
             {(showSuccess) && 
                 <Alert variant="success">
-                    Changes saved successfully
+                    Profile saved successfully.
                 </Alert>
             }
             <h4 className="profile-header">My Guide Profile</h4>
@@ -96,17 +107,17 @@ const Profile = (props) => {
                 <Form.Row>
                     <Form.Group as={Col} controlId="name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control maxlength="20" type="name" defaultValue={formState.name} isInvalid={nameError}/>
+                        <Form.Control maxlength="20" type="name" defaultValue={formState.name} /> {/*isInvalid={nameError}/>
                         <Form.Control.Feedback type="invalid">
                             Must be at least 3 characters.
-                        </Form.Control.Feedback>
+                        </Form.Control.Feedback>*/}
                     </Form.Group>
                     <Form.Group as={Col} controlId="mobile">
                         <Form.Label>Mobile</Form.Label>
-                        <Form.Control maxlength="15" type="text" isInvalid={mobileError} defaultValue={formState.mobile} />
+                        <Form.Control maxlength="15" type="text" defaultValue={formState.mobile} /> {/*isInvalid={mobileError} />
                         <Form.Control.Feedback type="invalid">
                             Must be at least 8 numeric digits.
-                        </Form.Control.Feedback>
+                        </Form.Control.Feedback>*/}
                     </Form.Group>
                     <Form.Group as={Col} controlId="email">
                         <Form.Label>Email Address</Form.Label>
