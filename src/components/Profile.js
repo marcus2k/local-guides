54e7ee638cd721a29d27c28fd10a76fcfcf390f1
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-21 08:01:48 +0800

    Remove gender options

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
 import Select from 'react-select';
 
-const genderOptions = [{label: "Male", value: "M"}, {label: "Female", value: "F"}];
+// const genderOptions = [{label: "Male", value: "M"}, {label: "Female", value: "F"}];
 
 const sortThenObjectify = lst => lst.sort((a, b) => a > b ? 1 : -1).map(x => ({value: x, label: x}));
 
@@ -26,7 +26,7 @@ const isValidCities = cities => cities.length > 0;
 
 const isValidLanguages = lang => lang.length > 0;
 
-const FORM_PROPS = [ "name", "intro", "gender", "currency", "hourlyRate", "email", "mobile", "transport", "cities", "languages" ];
+const FORM_PROPS = [ "name", "intro", "currency", "hourlyRate", "email", "mobile", "transport", "cities", "languages" ];
 
 const Profile = (props) => {
     const { currencies, citiesList } = props;
@@ -41,25 +41,28 @@ const Profile = (props) => {
         event.preventDefault();
         const form = event.target;
         // alert(form.name.value);
-        const [ name, intro, gender, currency, rate, email, mobile, transport ] = 
-            [ form.name, form.intro, form.gender, form.currency, form.hourlyRate, form.email, form.mobile, form.transport ]
+        const [ name, intro, currency, rate, email, mobile, transport ] = 
+            [ form.name, form.intro, form.currency, form.hourlyRate, form.email, form.mobile, form.transport ]
             .map(obj => obj.value);
         const [ cities, languages ] = [ form.cities, form.languages ]
-            .map(radioArr => radioArr.value ? radioArr.value : Array.from(radioArr).map(node => node.defaultValue));
+            .map(radioArr => radioArr.value ? [].concat(radioArr.value) : Array.from(radioArr).map(node => node.defaultValue));
         const newProfile = { 
             name: name, 
             intro: intro, 
-            gender: gender, 
+            // gender: gender, 
             hourlyRate: [currency, rate], 
             email: email, 
-            cities: [].concat(cities), 
-            languages: [].concat(languages),
+            cities: cities, 
+            languages: languages,
             mobile: mobile,
             transport: transport,
         };
+        name.length < 3 ? setNameError(true) : void(0);
+        cities.length ? void(0) : setCitiesError(true);
+        languages.length ? void(0) : setLangError(true);
         console.log(name);
         console.log(intro);
-        console.log(gender);
+        //console.log(!gender);
         console.log(currency);
         console.log(rate);
         console.log(email);
@@ -70,23 +73,23 @@ const Profile = (props) => {
 
     return (
         <>
-            <h2 className="city-header">My Guide Profile</h2>
+            <h4 className="profile-header">My Guide Profile</h4>
             <Form style={{margin: 30}} onSubmit={handleSubmit}>
                 <Form.Row>
                     <Form.Group as={Col} controlId="name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="name" defaultValue="currentName" isInvalid={true}/>
+                        <Form.Control maxlength="20" type="name" defaultValue="currentName" isInvalid={nameError}/>
                         <Form.Control.Feedback type="invalid">
-                            Test
+                            Must be at least 3 characters.
                         </Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group as={Col} controlId="intro">
+                    {/*<Form.Group as={Col} controlId="intro">
                         <Form.Label>Gender</Form.Label>
                         <Select
                         className="basic-single"
                         classNamePrefix="select"
-                        //placeholder="Select your gender"
-                        defaultValue="M"
+                        placeholder="Select gender"
+                        defaultValue={undefined}
                         isDisabled={false}
                         isLoading={false}
                         isClearable={true}
@@ -96,21 +99,24 @@ const Profile = (props) => {
                         options={genderOptions}
                         onChange={void(0)}
                         />
-                    </Form.Group>
+                    </Form.Group>*/}
                     <Form.Group as={Col} controlId="email">
                         <Form.Label>Email Address</Form.Label>
                         <Form.Control disabled type="email" defaultValue="currentEmail@example.com" />
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
-                    <Form.Row>
+                    <Form.Group as={Col} controlId="hourlyRate">
+                        <Form.Label>Hourly Rate</Form.Label>
+                        <Form.Control min="0" type="number" default={0} />
+                    </Form.Group>
                     <Form.Group as={Col} controlId="currency">
                         <Form.Label>Currency</Form.Label>
                         <Select
                         className="basic-single"
                         classNamePrefix="select"
                         //placeholder="Currency"
-                        defaultValue="USD"
+                        defaultValue={{label:"USD", value:"USD"}}
                         isDisabled={false}
                         isLoading={false}
                         isClearable={false}
@@ -121,29 +127,18 @@ const Profile = (props) => {
                         onChange={void(0)}
                         />
                     </Form.Group>
-                    <Form.Group as={Col} controlId="hourlyRate">
-                        <Form.Label>Hourly Rate</Form.Label>
-                        <Form.Control min="0" type="number" default={0} />
-                    </Form.Group>
                     <Form.Group as={Col} controlId="transport">
                     <Form.Label>Transport</Form.Label>
                     <Form.Control type="number" min="0" default={0} placeholder="No. of guests" />
                     </Form.Group>
-                    </Form.Row>
                     <Form.Group as={Col} controlId="mobile">
                         <Form.Label>Mobile</Form.Label>
                         <Form.Control type="tel" defaultValue="+6593847382" />
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
-                    <Form.Group as={Col} controlId="intro">
-                        <Form.Label>Brief Introduction</Form.Label>
-                        <Form.Control as="textarea" type="text" defaultValue={"currentIntroduction ".repeat(20)} />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
                     <Form.Group as={Col} controlId="cities">
-                        <Form.Label>Cities</Form.Label>
+                        <Form.Label >Cities</Form.Label>
                         <Select
                         className="basic-single"
                         classNamePrefix="select"
@@ -160,8 +155,8 @@ const Profile = (props) => {
                         onChange={void(0)}
                         />
                     </Form.Group>
-                </Form.Row>
-                <Form.Row>
+                {/*</Form.Row>
+                <Form.Row>*/}
                     <Form.Group as={Col} controlId="languages">
                         <Form.Label>Languages</Form.Label>
                         <Select
@@ -180,6 +175,12 @@ const Profile = (props) => {
                         onChange={void(0)}
                         />
                     </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                <Form.Group as={Col} controlId="intro">
+                    <Form.Label>Brief Introduction</Form.Label>
+                    <Form.Control maxlength="200" as="textarea" type="text" defaultValue={"dummy ".repeat(5)} />
+                </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group as={Col}>
