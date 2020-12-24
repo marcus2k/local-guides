b3b0b241360f152f138cc6055fcbeeb8104c0d24
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-25 04:51:16 +0800

    Remove id field and set email as key

--- react/src/components/Profile.js
+++ react/src/components/Profile.js
@@ -15,13 +15,12 @@ const isValidName = name => ![1, 2].includes(name.length);
 
 const isValidMobile = mobile => (mobile.match(/^[0-9]+$/) && mobile.length > 7) || mobile.length === 0;
 
-const FORM_PROPS = [ "id", "name", "intro", "currency", "hourlyRate", "email", "mobile", "transport", "cities", "languages" ];
+const FORM_PROPS = [ "name", "intro", "currency", "hourlyRate", "email", "mobile", "transport", "cities", "languages" ];
 
 const Required = () => <span className="required">*</span>;
 
 const BLANK_STATE = email =>
 ({
-    id: "",
     name: "",
     gender: "",
     cities: [],
@@ -119,7 +118,6 @@ const Profile = (props) => {
         const [ cities, languages ] = [ form.cities, form.languages ]
             .map(radioArr => radioArr.value ? [].concat(radioArr.value) : Array.from(radioArr).map(node => node.defaultValue));
         const newProfile = { 
-            id: "0",
             name: name, 
             intro: intro, 
             hourlyRate: [currency, rate], 
